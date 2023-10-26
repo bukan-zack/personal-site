9c1aa092a1557cdca5c50cae6645cb79b31c0c78
Zackry Rosli <zackry@berrybyte.net>
2023-10-26 23:04:57 +0800

    Make footer text-xs

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@
             <RouterView v-slot="{ Component }">
                 <component :is="Component" />
             </RouterView>
-            <footer class="mt-8 text-zinc-500">
+            <footer class="mt-8 text-zinc-500 text-xs">
                 &copy; 2023 Zackry Rosli. All rights reserved.
             </footer>
         </div>
